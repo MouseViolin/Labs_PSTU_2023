f6ddaf7041131b5f70432683b110e41e0f6fd1c3
ych <lalapoplars@gmail.com>
2023-09-28 22:08:34 +0500

    O.o

--- GitDemo/main.cpp
+++ GitDemo/main.cpp
@@ -10,6 +10,8 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, cake!\n";
+    std::cout << "Help me, pls\n";
+    std::cout << "I'm tired\n";
     return 0;
 }
-// add code
+// add O.o
