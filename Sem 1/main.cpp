13bd522b32d5045ba6a08d1f2399f52f73f26e3f
ych <lalapoplars@gmail.com>
2023-11-27 23:59:03 +0500

    block diagram 2_10

--- Sem 1/main.cpp
+++ Sem 1/main.cpp
@@ -3,100 +3,16 @@ using namespace std;
 
 int main()
 {
-    int n;
+    int n,k, max_first = 0;
+    
     cin >> n;
-    int spaces =(n/2)-1;
-    int after_edge = 0;
-//     начало первого горизонтального ребра с учетом смещения
-    for (int j = 0; j < spaces; j++)
-    {
-        cout << "  ";
-    }
-    spaces--;
-    cout << "*";
-//     заполнение первого горизонтального ребра
-    for (int j = 0; j < n-2; j++)
-    {
-        cout << " *";
-    }
-    cout << "  *";
-    cout << endl;
-//      ребра по диагонали
-    for (int i = 0; i < n/2-2; i++)
-    {
-        for (int j = 0; j < spaces; j++)
-        {
-            cout << "  ";
+    for (int i = 0; i < n; i++) {
+        cin >> k;
+        if (k > max_first) {
+            max_first = k;
         }
-        spaces--;
-        after_edge += 2;
-        cout << "*";
-        for (int j = 0; j < n-1; j++)
-        {
-            cout << "  ";
-        }
-        cout << "*";
-//        заднее вертикальное ребро
-        for (int j = 0; j < after_edge; j++)
-        {
-            cout << " ";
-        }
-        cout << "*";
-        cout << endl;
     }
-//    переднее верхнее ребро
-    cout << "* ";
-    for (int j = 0; j < n-2; j++)
-    {
-        cout << "* ";
-    }
-    cout << "*";
-//    продолжение заднего вертикального ребра
-    for (int j = 0; j < after_edge+2; j++)
-    {
-        cout << " ";
-    }
-    cout << " *";
-    cout << endl;
-//    передняя грань с задним ребром ДО начала ребра по вертикали
-    for (int i = 0; i < n/2-1; i++)
-    {
-        cout << "*";
-        for (int j = 0; j < n-2; j++){
-            cout << "  ";
-        }
-        cout << " *";
-        for (int j = 0; j < after_edge+2; j++)
-        {
-            cout << " ";
-        }
-        cout << " *";
-        cout << endl;
-    }
-//    передняя грань с задним ребром ПОСЛЕ начала ребра по вертикали
-    for (int i = 0; i < n/2-1; i++)
-    {
-        cout << "*";
-        for (int j = 0; j < n-2; j++){
-            cout << "  ";
-        }
-        cout << " *";
-        
-        for (int j = 0; j < after_edge+2; j++)
-        {
-            cout << " ";
-        }
-            
-        cout << " *";
-        after_edge -= 2;
-        cout << endl;
-    }
-//    последнее горизонтальное ребро
-    for (int j = 0; j < n; j++)
-    {
-        cout << "* ";
-    }
-    cout << endl;
+    cout << max_first << endl;
     return 0;
 }
 
