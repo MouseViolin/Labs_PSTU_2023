a9f14efb4282b11d14434e7e24d8b7b7eaa2a1a2
ych <lalapoplars@gmail.com>
2024-02-05 12:52:14 +0500

    code for task 3_7

---
+++ Sem 2/3_7/3_7.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main() {
+    srand(time(0));
+    int n, max, min;
+    cin >> n;
+    int array[n];
+    int lb = 20, ub = 100;
+    
+    
+    for (int j = 0; j < n; j++) {
+        array[j] = (rand() % (ub - lb +1)) + lb;
+        cout << array[j] << " " ;
+    }
+    cout << endl;
+    max = array[0];
+    min = array[0];
+    for (int i = 1; i <n; i++) {
+        if (max < array[i]) {
+            max = array[i];
+        } else if (min > array[i]){min = array[i];}
+    }
+    
+    cout << max << endl << min << endl;
+    return 0;
+}
